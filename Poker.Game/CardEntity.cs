ec70565270ee409361b5e0bf79f62de1d0d340f2
Connor Conway <connor_conway1@outlook.com>
2018-03-12 13:09:15 +0000

    [ch30] Start using monogame to create the GUI. Currently just displaying an entire spritesheet

---
+++ Poker.Game/CardEntity.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+
+namespace Poker.Game
+{
+	public class CardEntity
+	{
+		static Texture2D cardsSheetTexture;
+
+		public float X
+		{
+			get;
+			set;
+		}
+
+		public float Y
+		{
+			get;
+			set;
+		}
+
+		public CardEntity(GraphicsDevice graphicsDevice)
+		{
+			if (cardsSheetTexture == null)
+			{
+				using (var stream = TitleContainer.OpenStream("Content/cards.png"))
+				{
+					cardsSheetTexture = Texture2D.FromStream(graphicsDevice, stream);
+				}
+			}
+		}
+
+		public void Draw(SpriteBatch spriteBatch)
+		{
+			Vector2 topLeftOfSprite = new Vector2(this.X, this.Y);
+			Color tintColor = Color.White;
+			spriteBatch.Draw(cardsSheetTexture, topLeftOfSprite, tintColor);
+		}
+	}
+}
---
+++ Poker.Game/Game1.cs
@@ -0,0 +1,87 @@
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Poker.Game
+{
+	/// <summary>
+	/// This is the main type for your game.
+	/// </summary>
+	public class Game1 : Microsoft.Xna.Framework.Game
+	{
+		GraphicsDeviceManager graphics;
+		SpriteBatch spriteBatch;
+
+		private CardEntity _cardEntity;
+
+
+		public Game1()
+		{
+			graphics = new GraphicsDeviceManager(this);
+			Content.RootDirectory = "Content";
+
+			graphics.IsFullScreen = true;
+			graphics.PreferredBackBufferWidth = 800;
+			graphics.PreferredBackBufferHeight = 480;
+			graphics.SupportedOrientations = DisplayOrientation.LandscapeLeft | DisplayOrientation.LandscapeRight;
+		}
+
+		/// <summary>
+		/// Allows the game to perform any initialization it needs to before starting to run.
+		/// This is where it can query for any required services and load any non-graphic
+		/// related content.  Calling base.Initialize will enumerate through any components
+		/// and initialize them as well.
+		/// </summary>
+		protected override void Initialize()
+		{
+			_cardEntity = new CardEntity(this.GraphicsDevice);
+			base.Initialize();
+		}
+
+		/// <summary>
+		/// LoadContent will be called once per game and is the place to load
+		/// all of your content.
+		/// </summary>
+		protected override void LoadContent()
+		{
+			spriteBatch = new SpriteBatch(GraphicsDevice);
+		}
+
+		/// <summary>
+		/// UnloadContent will be called once per game and is the place to unload
+		/// game-specific content.
+		/// </summary>
+		protected override void UnloadContent()
+		{
+			// TODO: Unload any non ContentManager content here
+		}
+
+		/// <summary>
+		/// Allows the game to run logic such as updating the world,
+		/// checking for collisions, gathering input, and playing audio.
+		/// </summary>
+		/// <param name="gameTime">Provides a snapshot of timing values.</param>
+		protected override void Update(GameTime gameTime)
+		{
+			if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed)
+				Exit();
+
+			// TODO: Add your update logic here
+
+			base.Update(gameTime);
+		}
+
+		/// <summary>
+		/// This is called when the game should draw itself.
+		/// </summary>
+		/// <param name="gameTime">Provides a snapshot of timing values.</param>
+		protected override void Draw(GameTime gameTime)
+		{
+			GraphicsDevice.Clear(Color.CornflowerBlue);
+			spriteBatch.Begin();
+			_cardEntity.Draw(spriteBatch);
+			spriteBatch.End();
+			base.Draw(gameTime);
+		}
+	}
+}
