07a9ab22b6811f28b1bec03a9c8366f8538fe13e
Connor Conway <connor_conway1@outlook.com>
2018-05-02 16:30:10 +0100

    [ch41] Change table positions

--- Poker.Game/TextureHandling/Positions.cs
+++ Poker.Game/TextureHandling/Positions.cs
@@ -9,7 +9,7 @@ namespace Poker.Game.TextureHandling
 		public static Vector2 TableLeft => new Vector2 {X = 140, Y = 320};
 		public static Vector2 TableBottom => new Vector2 {X = 550, Y = 510};
 		public static Vector2 TableMiddleRight => new Vector2 {X = 750, Y = 320};
-		public static Vector2 TableMiddleLeft => new Vector2 {X = 250, Y = 320};
+		public static Vector2 TableMiddleLeft => new Vector2 {X = 500, Y = 320};
 		public static Vector2 BelowTable => new Vector2 {X = 560, Y = 680};
 	}
 }
